9c700019c133b7f9eea1d7f91e862f27b445a303
mazmann <sadi.lundberg@hotmail.com>
2024-09-05 17:47:29 +0200

    css

--- src/components/nestable/Button.jsx
+++ src/components/nestable/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ blok }) {
     return (
       <a
         href={blok?.link}
-        className="inline-block mt-4 px-6 py-3 bg-black text-white font-medium text-sm rounded-none hover:bg-gray-800"
+        className="inline-block mt-4 px-6 py-3 bg-black text-white font-medium text-sm rounded-none hover:bg-gray-800 text-center"
       >
         {blok?.button_text}
       </a>
