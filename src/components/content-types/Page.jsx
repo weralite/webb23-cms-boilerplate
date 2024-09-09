3abd69318146e58660f8261be1ed1814a87faad8
mazmann <sadi.lundberg@hotmail.com>
2024-09-09 15:38:59 +0200

    width capped 1400

--- src/components/content-types/Page.jsx
+++ src/components/content-types/Page.jsx
@@ -3,7 +3,7 @@ import { StoryblokComponent } from "@storyblok/react/rsc";
 export default function Page({ blok }) {
     return (
         <main className="flex flex-col items-center justify-center min-h-[80vh] w-full">
-            <div className="relative w-full max-w-[1500px]">
+            <div className="relative w-full max-w-[1400px]">
             {blok?.body?.map((blok) => (
                 <StoryblokComponent blok={blok} key={blok._uid} />
             ))}
